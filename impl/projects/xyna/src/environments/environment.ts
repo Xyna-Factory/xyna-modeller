cb3785d37d82669f8ec5088821f9e5e05ea5ea64
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2024-11-05 12:52:25 +0100

    upgrad Angular v18

--- impl/projects/xyna/src/environments/environment.ts
+++ impl/projects/xyna/src/environments/environment.ts
@@ -27,7 +27,7 @@ import { RuntimeContext } from '@zeta/api/xo/xo-describer';
 export const environment: ZetaEnvironment = {
     production: false,
     zeta: {
-        url: 'https://host-ip-address:host-port/modeller-api/',
+        url: 'https://10.0.10.191/modeller-api/',
         xo: {
             runtimeContext: RuntimeContext.fromApplication('GuiHttp'),
             consistencyCheck: false
