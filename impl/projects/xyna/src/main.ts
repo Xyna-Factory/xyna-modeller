be29f3136d05ff6952bc8b34d916046c991c78f5
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2025-12-02 18:12:20 +0100

    update editor

--- impl/projects/xyna/src/main.ts
+++ impl/projects/xyna/src/main.ts
@@ -21,7 +21,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { environment } from '@environments/environment';
 
 import { AppModule } from './app/app.module';
-import './app/zeta/monaco-environment';
 
 
 if (environment.production) {
